2b82995db84ac26765cc4e319fedb4dc41ee4f76
Felix Kopp <felix@thinkwrong.de>
2019-10-17 16:00:28 +0300

    First modifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'psoas-dark-grey': '#3C3B3C',
+        'psoas-light-grey': 'grey',
+        'psoas-green': '#37B322',
+        'psoas-green-hover': '#2f9a1d',
+        'psoas-white': '#fafafa',
+        'psoas-orange': '#E8703B',
+        'psoas-blue-own': '#1347B2',
+        'psoas-blue-third': '#28417F',
+        'psoas-black': '#0a0a0a',
+      }
+    },
   },
   variants: {},
+  corePlugins: {
+    tableLayout: false,
+  },
   plugins: [],
 }
